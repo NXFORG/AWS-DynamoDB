182773e8a23776092e36e6143669ef5e8c2c83f8
Chris Anstead <37987393+NXFORG@users.noreply.github.com>
2022-04-27 11:02:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.patch('/books/:id', async (req, res) => {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
